209b4559b089ef1df166b0b3d4bda3a1318eea51
Jacob <jac.snall@gmail.com>
2022-02-06 17:07:10 +0100

    Added copyWith method to puzzlestate

--- lib/puzzle/cubit/puzzle_cubit.dart
+++ lib/puzzle/cubit/puzzle_cubit.dart
@@ -3,35 +3,42 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:equatable/equatable.dart';
 
+import 'package:jac_slide_puzzle/models/models.dart';
+
 part 'puzzle_state.dart';
 
 class PuzzleCubit extends Cubit<PuzzleState> {
   PuzzleCubit()
       : super(
-          PuzzleState(transformation: Matrix4.identity()),
+          PuzzleState(
+            puzzle: Puzzle(),
+            transformation: Matrix4.identity(),
+          ),
         );
 
   void rotateLeft() {
-    emit(PuzzleState(
-        transformation: Matrix4.rotationY(pi / 2)
-          ..multiply(state.transformation)));
+    emit(state.copyWith(
+      transformation: Matrix4.rotationY(pi / 2)..multiply(state.transformation),
+    ));
   }
 
   void rotateRight() {
-    emit(PuzzleState(
-        transformation: Matrix4.rotationY(-pi / 2)
-          ..multiply(state.transformation)));
+    emit(state.copyWith(
+      transformation: Matrix4.rotationY(-pi / 2)
+        ..multiply(state.transformation),
+    ));
   }
 
   void rotateUp() {
-    emit(PuzzleState(
-        transformation: Matrix4.rotationX(-pi / 2)
-          ..multiply(state.transformation)));
+    emit(state.copyWith(
+      transformation: Matrix4.rotationX(-pi / 2)
+        ..multiply(state.transformation),
+    ));
   }
 
   void rotateDown() {
-    emit(PuzzleState(
-        transformation: Matrix4.rotationX(pi / 2)
-          ..multiply(state.transformation)));
+    emit(state.copyWith(
+      transformation: Matrix4.rotationX(pi / 2)..multiply(state.transformation),
+    ));
   }
 }
--- lib/puzzle/cubit/puzzle_state.dart
+++ lib/puzzle/cubit/puzzle_state.dart
@@ -1,13 +1,30 @@
 part of 'puzzle_cubit.dart';
 
 class PuzzleState extends Equatable {
-  PuzzleState({required this.transformation});
+  PuzzleState({
+    required this.transformation,
+    required this.puzzle,
+  });
 
   final Matrix4 transformation;
   final Matrix4 skew = Matrix4.identity()
     ..rotateX(pi / 16)
     ..rotateY(pi / 16);
 
+  // Puzzle model
+  final Puzzle puzzle;
+
+  //Copies the Puzzle state
+  PuzzleState copyWith({
+    Matrix4? transformation,
+    Puzzle? puzzle,
+  }) {
+    return PuzzleState(
+      transformation: transformation ?? this.transformation,
+      puzzle: puzzle ?? this.puzzle,
+    );
+  }
+
   @override
-  List<Object> get props => [transformation];
+  List<Object> get props => [transformation, puzzle];
 }
